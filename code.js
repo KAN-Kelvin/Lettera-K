bb955c0ee9795e3af6fdfb286d90a06d61754ca3
KAN-Kelvin <kanwenxuan1999@gmail.com>
2025-04-01 13:13:22 +0200

    Update code.js

--- code.js
+++ code.js
@@ -32,44 +32,16 @@ export function disegnaPunto({
   push();
   translate(x, y);
   rotate(frameCount);
-  strokeWeight(6);
+  image(img, 0, 0, 40, 40);
 
-  if (indice % 2 == 0) fill("grey");
-  else fill("orange");
-  ellipse(0, 0, sin(frameCount) * 30);
-
-  // rectMode(CENTER);
-  // rect(x, y, volume * 200, 20);
-
-  // noFill();
-  // stroke(0);
-
-  // // Use orientation data to influence color
-  // // Map alpha (z-rotation) to hue (0-360)
-  // const hue = map(alpha, 0, 360, 0, 360);
-
-  // // Map beta (front-to-back tilt) to saturation (50-100)
-  // const saturation = map(abs(beta), 0, 90, 50, 100);
-
-  // // Map gamma (left-to-right tilt) to brightness (50-100)
-  // const brightness = map(abs(gamma), 0, 90, 50, 100);
-
-  // colorMode(HSB, 360, 100, 100);
-  // fill(hue, saturation, brightness);
-  // noStroke();
-
-  // rectMode(CENTER);
-  // rotate(frameCount + indice);
-
-  // // Add slight variation based on device tilt
-  // scale(1 + volume * 10 + (abs(gamma) / 90) * 0.5);
-  // rect(0, 0, unita / 2);
   pop();
 }
 
 //
-
-export function caricamentoRisorse() {}
+let img;
+export function caricamentoRisorse() {
+  img = loadImage("./assets/image.png");
+}
 
 export function impostazioni() {
   frameRate(30);
